e7576fd2608af9f637cbab5f799399c930bf6d63
Andrew Pieschke <ampieschke@gmail.com>
2021-07-29 11:26:16 -0400

    refined routing for a more seemless add and delete experience

--- app/public/db.js
+++ app/public/db.js
@@ -1,24 +1,24 @@
-let db;
+// let db;
 
-const request = indexedDB.open("checklist", 1);
+// const request = indexedDB.open("checklist", 1);
 
-request.onupgradeneeded = function (event) {
-  const db = event.target.result;
-  db.createObjectStore("pending", { autoIncrement: true });
-};
+// request.onupgradeneeded = function (event) {
+//   const db = event.target.result;
+//   db.createObjectStore("pending", { autoIncrement: true });
+// };
 
-request.onsuccess = function (event) {
-  db = event.target.result;
-  if (navigator.onLine) {
-  }
-};
+// request.onsuccess = function (event) {
+//   db = event.target.result;
+//   if (navigator.onLine) {
+//   }
+// };
 
-request.onerror = function (event) {
-  console.log("Woops! " + event.target.errorCode);
-};
+// request.onerror = function (event) {
+//   console.log("Woops! " + event.target.errorCode);
+// };
 
-function saveRecord(record) {
-  const item = db.transaction(["pending"], "readwrite");
-  const store = item.objectStore("pending");
-  store.add(record);
-}
+// function saveRecord(record) {
+//   const item = db.transaction(["pending"], "readwrite");
+//   const store = item.objectStore("pending");
+//   store.add(record);
+// }
--- app/public/index.js
+++ app/public/index.js
@@ -1,5 +1,3 @@
-let items = [];
-
 function getResults() {
   clearTodos();
   fetch("/api/item")
@@ -11,7 +9,7 @@ function getResults() {
         return;
       }
       response.json().then(function (data) {
-        populateChecklist(data);
+        newItemSnippet(data);
       });
     })
     .catch(function (err) {
@@ -19,21 +17,10 @@ function getResults() {
     });
 }
 
-function populateChecklist(res) {
-  // const theList = document.querySelector("#theList");
-  // theList.innerHTML = "";
-  // items.forEach((item) => {
-  //   const pi = document.createElement("pi");
-  //   pi.innerHTML = `
-  //     <span>${item.name}<button class="delete" id=${item._id}>Check</button></span><hr><br>
-  //   `;
-
-  //   theList.appendChild(pi);
-  // });
-
-  for (var i = 0; i < res.length; i++) {
-    let data_id = res[i]["_id"];
-    let name = res[i]["name"];
+function newItemSnippet(response) {
+  for (var i = 0; i < response.length; i++) {
+    let data_id = response[i]["_id"];
+    let name = response[i]["name"];
     let todoList = document.getElementById("theList");
     snippet = `
     <p class="data-entry">
@@ -48,41 +35,40 @@ function clearTodos() {
   const todoList = document.getElementById("theList");
   todoList.innerHTML = "";
 }
-
 getResults();
 
-function sendItem() {
-  const nameEl = document.querySelector("#td").value;
-  console.log(nameEl);
+// function sendItem() {
+//   const nameEl = document.querySelector("#td").value;
+//   console.log(nameEl);
 
-  // create record
-  const item = {
-    name: nameEl,
-    done: false,
-    date: new Date().toISOString(),
-  };
+//   // create record
+//   const item = {
+//     name: nameEl,
+//     done: false,
+//     date: new Date().toISOString(),
+//   };
 
-  // add to beginning of current array of data
-  items.unshift(item);
+//   // add to beginning of current array of data
+//   // items.unshift(item);
 
-  // re-run logic to populate ui with new record
-  populateChecklist();
+//   // re-run logic to populate ui with new record
+//   // populateChecklist();
 
-  // also send to server
-  fetch("/api/item", {
-    method: "POST",
-    body: JSON.stringify(item),
-    headers: {
-      Accept: "application/json, text/plain, */*",
-      "Content-Type": "application/json",
-    },
-  })
-    .then((response) => response.json())
-    .catch((err) => {
-      // fetch failed, so save in indexed db
-      sendItem(item);
-    });
-}
+//   // also send to server
+//   fetch("/api/item", {
+//     method: "POST",
+//     body: JSON.stringify(item),
+//     headers: {
+//       Accept: "application/json, text/plain, */*",
+//       "Content-Type": "application/json",
+//     },
+//   })
+//     .then((response) => response.json())
+//     .catch((err) => {
+//       // fetch failed, so save in indexed db
+//       sendItem(item);
+//     });
+// }
 
 theList.addEventListener("click", function (e) {
   if (e.target.matches(".delete")) {
@@ -105,10 +91,25 @@ theList.addEventListener("click", function (e) {
         console.log("Fetch Error :-S", err);
       });
   }
-  populateChecklist();
 });
 
 document.querySelector("#add-btn").addEventListener("click", function (event) {
   event.preventDefault();
-  sendItem(true);
+  // sendItem(true);
+  element = event.target;
+  data_id = element.getAttribute("data-id");
+  fetch("/api/item", {
+    method: "post",
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: document.getElementById("td").value,
+      done: false,
+      created: Date.now(),
+    }),
+  })
+    .then((res) => res.json())
+    .then((res) => newItemSnippet([res]));
 });
