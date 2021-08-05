12e4bfbab31dc61a06ec461ee89fc49c0e4e9ad5
Andrew Pieschke <ampieschke@gmail.com>
2021-08-05 14:45:06 -0400

    updated styling for mobile friendly experience

--- app/public/index.js
+++ app/public/index.js
@@ -147,3 +147,16 @@ document.querySelector("#add-btn").addEventListener("click", function (event) {
   const addSpot = document.getElementById("td");
   addSpot.value = "";
 });
+
+//Input Animation
+
+$("input").focus(function (event) {
+  $(this).closest(".float-label-field").addClass("float").addClass("focus");
+});
+
+$("input").blur(function () {
+  $(this).closest(".float-label-field").removeClass("focus");
+  if (!$(this).val()) {
+    $(this).closest(".float-label-field").removeClass("float");
+  }
+});
